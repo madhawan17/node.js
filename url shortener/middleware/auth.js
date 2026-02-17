6348bc3622df6b203b8bc88476ed6e50fc724a53
madhawan17 <aryamadhawan@gmail.com>
2026-02-17 23:32:36 +0530

    headers-method

--- url shortener/middleware/auth.js
+++ url shortener/middleware/auth.js
@@ -2,10 +2,11 @@ const { getUser } = require("../service/auth");
 
 async function restrictToLoggedInUserOnly(req,res,next)  {
     // console.log(req);
-    const userUid = req.cookies?.uid;
+    const userUid = req.headers['Authorization'];
 
     if (!userUid) return res.redirect("/login");
-    const user = getUser(userUid);
+    const token = userUid.split("Bearer")[1];
+    const user = getUser(token);
 
     if (!user) return res.redirect("/login");
 
@@ -14,9 +15,11 @@ async function restrictToLoggedInUserOnly(req,res,next)  {
 }
 
 async function checkAuth(req,res,next)  {
-    const userUid = req.cookies?.uid;
+    const userUid = req.headers['Authorization'];
+    const token = userUid.split("Bearer")[1];
+     
 
-    const user = getUser(userUid);
+    const user = getUser(token);
 
     req.user = user;
     next()
