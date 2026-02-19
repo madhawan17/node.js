d7fcb67847a32745dfc720a8d82159400b3db447
madhawan17 <aryamadhawan@gmail.com>
2026-02-19 23:28:14 +0530

    web-socket start

---
+++ chat-app/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const 
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req,res) => {
+    return res.sendFile("./public/index.html");
+})
+
+server.listen(9000, () => console.log(`Server started at port 9000`));
