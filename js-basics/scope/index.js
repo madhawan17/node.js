39eb323bc341a58f6f8cc6c1e7b9b425d06d8b7d
madhawan17 <aryamadhawan@gmail.com>
2026-02-24 23:24:57 +0530

    closures

--- js-basics/scope/index.js
+++ js-basics/scope/index.js
@@ -22,3 +22,19 @@ console.log(a);
 
 a = "satyam";
 console.log(a);
+
+// closures
+
+function x() {
+  var a = 7;
+  
+  function y() {
+    console.log(a);
+  }
+  
+  return y;
+}
+
+var z = x();
+console.log(z);
+z();
