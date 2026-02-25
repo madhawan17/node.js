91c2bb0f354ffc542a7788ed1e338348f1cf2fa0
madhawan17 <aryamadhawan@gmail.com>
2026-02-25 23:37:49 +0530

    block-scope

--- js-basics/scope/index.js
+++ js-basics/scope/index.js
@@ -38,3 +38,32 @@ function x() {
 var z = x();
 console.log(z);
 z();
+
+// block scope
+// First: What is a Block?
+
+// A block is:
+
+{
+   // group of statements
+}
+
+Example:
+
+{
+   var a = 10;
+   let b = 20;
+   const c = 30;
+}
+
+// This {} is called a Block or Compound Statement.
+
+// 🔥 Why Do We Need Blocks?
+
+// Blocks allow us to:
+
+// Group multiple statements
+
+// Use if, for, while, etc.
+
+// Create block-level scope (with let & const)
