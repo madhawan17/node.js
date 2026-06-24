5dd6ac3bb6bf92073bd4c22018680147b7f63810
madhawan17 <aryamadhawan@gmail.com>
2026-06-24 21:56:59 +0530

    refactor: update error handler logger path and enhance server setup with rate limiting and error handling

--- microservices/identity-service/src/routes/identity-service.js
+++ microservices/identity-service/src/routes/identity-service.js
@@ -1,8 +1,12 @@
+console.log("IDENTITY ROUTES LOADED");
+
 const express = require('express');
 const {registerUser} = require('../controller/identity-controller');
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
+
 
 module.exports = router;
--- microservices/identity-service/src/server.js
+++ microservices/identity-service/src/server.js
@@ -1,10 +1,20 @@
 require('dotenv').config()
 
+const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const helmet = require('helmet');
 const cors = require('cors');
 const {RateLimiterRedis} = require('rate-limiter-flexible');
+const Redis = require('ioredis');
+const routes = require('./routes/identity-service');
+const errorHandler = require('./middleware/errorHandler');
+const {rateLimit} = require('express-rate-limit');
+const {RedisStore} = require('rate-limit-redis');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 // connect to mongodb
@@ -14,7 +24,7 @@ mongoose
     .then(()=> logger.info('connected to mongodb'))
     .catch((e) => logger.info('mongodb connection error', e));
 
-
+    const redisClient = new Redis(process.env.REDIS_URL)
 
 // middleware
 
@@ -28,3 +38,57 @@ app.use((req,res,next) => {
     logger.info(`Request body, ${req.body}`);
     next();
 });
+
+
+// DDos protection and rate limiting 
+const rateLimiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: 'middleware',
+    points : 10,
+    duration: 1
+});
+
+app.use((req,res,next) => {
+    rateLimiter
+        .consume(req.ip)
+        .then(() => next())
+        .catch(() => {
+            logger.warn(`Rate limit exceed for ip: ${req.ip}`);
+            res.status(429).json({success: false, message: 'too many request'});
+        });
+});
+
+// ip based rate limiting for sensitive endpoints
+const sensitiveEndpointsLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP: ${req.ip}`);
+    res.status(429).json({ success: false, message: "Too many requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+//apply this sensitiveEndpointsLimiter to our routes
+app.use("/api/auth/register", sensitiveEndpointsLimiter);
+
+//Routes
+app.use("/api/auth", routes);
+
+//error handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Identity service running on port ${PORT}`);
+});
+
+//unhandled promise rejection
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled Rejection at", promise, "reason:", reason);
+});
+
